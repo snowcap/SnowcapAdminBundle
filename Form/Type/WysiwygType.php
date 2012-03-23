1e722fe76195bc4c9c2b5211d1d399df2b51b7f7
Pierre Vanliefland <pierre@snowcap.be>
2012-03-23 12:03:45 +0100

    CKEditor implementation

---
+++ Form/Type/WysiwygType.php
@@ -0,0 +1,56 @@
+<?php
+namespace Snowcap\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+
+/**
+ * Markdown field type class
+ * 
+ */
+class WysiwygType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'wysiwyg';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultOptions(array $options) {
+        return array(
+            'style_file' => 'bundles/snowcapadmin/vendor/ckeditor/plugin/styles/styles/ckeditor_styles.js'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilder $builder, array $options) {
+        $builder->setAttribute('style_file', $options['style_file']);
+        parent::buildForm($builder, $options);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form)
+    {
+        $view->set('style_file', $form->getAttribute('style_file'));
+        parent::buildView($view, $form);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent(array $options)
+    {
+        return 'textarea';
+    }
+}
