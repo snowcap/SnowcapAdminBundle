9dcfb0867eb138bb75aaeb7cec0394876a8c961e
Edwin Hermans <edwin@snowcap.be>
2012-04-05 16:09:38 +0200

    CKEditor: converting width&height style attributes into html attributes

--- Resources/public/js/ckeditor.js
+++ Resources/public/js/ckeditor.js
@@ -27,6 +27,47 @@ jQuery(document).ready(function ($) {
         }
     });
 
+    CKEDITOR.on('instanceReady', function (ev) {
+        // Ends self closing tags the HTML4 way, like <br>.
+        ev.editor.dataProcessor.htmlFilter.addRules(
+            {
+                elements:{
+                    $:function (element) {
+                        // Output dimensions of images as width and height
+                        if (element.name == 'img') {
+                            var style = element.attributes.style;
+
+                            if (style) {
+                                // Get the width from the style.
+                                var match = /(?:^|\s)width\s*:\s*(\d+)px/i.exec(style),
+                                    width = match && match[1];
+
+                                // Get the height from the style.
+                                match = /(?:^|\s)height\s*:\s*(\d+)px/i.exec(style);
+                                var height = match && match[1];
+
+                                if (width) {
+                                    element.attributes.style = element.attributes.style.replace(/(?:^|\s)width\s*:\s*(\d+)px;?/i, '');
+                                    element.attributes.width = width;
+                                }
+
+                                if (height) {
+                                    element.attributes.style = element.attributes.style.replace(/(?:^|\s)height\s*:\s*(\d+)px;?/i, '');
+                                    element.attributes.height = height;
+                                }
+                            }
+                        }
+
+                        if (!element.attributes.style)
+                            delete element.attributes.style;
+
+                        return element;
+                    }
+                }
+            }
+        );
+    });
+
     /**
      * Common config options
      */
