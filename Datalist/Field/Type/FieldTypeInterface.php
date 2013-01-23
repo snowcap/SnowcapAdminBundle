2ca362b99904c4c507818bd303e3125746fb2ff6
Edwin Hermans <edwin@snowcap.be>
2013-01-23 10:18:12 +0100

    Corrected Field Type Interface, so that it does not redeclare the setDefaultOptions method

--- Datalist/Field/Type/FieldTypeInterface.php
+++ Datalist/Field/Type/FieldTypeInterface.php
@@ -2,19 +2,12 @@
 
 namespace Snowcap\AdminBundle\Datalist\Field\Type;
 
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-
 use Snowcap\AdminBundle\Datalist\TypeInterface;
 use Snowcap\AdminBundle\Datalist\Field\DatalistFieldInterface;
 use Snowcap\AdminBundle\Datalist\ViewContext;
 
 interface FieldTypeInterface extends TypeInterface
 {
-    /**
-     * @param \Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver);
-
     /**
      * @param \Snowcap\AdminBundle\Datalist\ViewContext $viewCobtext
      * @param \Snowcap\AdminBundle\Datalist\Field\DatalistFieldInterface $field
