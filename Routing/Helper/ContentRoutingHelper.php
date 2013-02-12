13486245434d3da51e8faf65bc8fd5493e979a90
pvanliefland <pierre@snowcap.be>
2013-02-12 12:03:27 +0100

    Changed Admin routing loader so that it attempts to match a custom controller before falling back to the default one

--- Routing/Helper/ContentRoutingHelper.php
+++ Routing/Helper/ContentRoutingHelper.php
@@ -2,6 +2,7 @@
 
 namespace Snowcap\AdminBundle\Routing\Helper;
 
+use Symfony\Bundle\FrameworkBundle\Controller\ControllerNameParser;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Routing\Route;
 
@@ -12,6 +13,11 @@ class ContentRoutingHelper {
      * @var \Symfony\Component\Routing\RouterInterface
      */
     private $router;
+
+    /**
+     * @var \Symfony\Bundle\FrameworkBundle\Controller\ControllerNameParser
+     */
+    private $parser;
 
     /**
      * @var string
@@ -27,8 +33,9 @@ class ContentRoutingHelper {
      * @param string $routeNamePrefix
      * @param string $routePrefix
      */
-    public function __construct(RouterInterface $router, $routePrefix, $routeNamePrefix) {
+    public function __construct(RouterInterface $router, ControllerNameParser $parser, $routePrefix, $routeNamePrefix) {
         $this->router = $router;
+        $this->parser = $parser;
         $this->routePrefix = $routePrefix;
         $this->routeNamePrefix = $routeNamePrefix;
     }
@@ -38,6 +45,18 @@ class ContentRoutingHelper {
         return $this->routeNamePrefix . '_' . $admin->getAlias() . '_' . $action;
     }
 
+    /**
+     * Build a route for the given admin and action, with the provided params
+     *
+     * This method will first attempt to find a custom Route (like "YourCustomAdminBundle:Section:index")
+     * and if it does not work
+     *
+     * @param \Snowcap\AdminBundle\Admin\ContentAdmin $admin
+     * @param string $action
+     * @param array $params
+     * @param bool $defaultRoute
+     * @return \Symfony\Component\Routing\Route
+     */
     public function getRoute(ContentAdmin $admin, $action, $params = array(), $defaultRoute = false)
     {
         $pattern = '/' . $admin->getAlias();
@@ -48,7 +67,18 @@ class ContentRoutingHelper {
             $pattern .= '/{' . $param . '}';
         }
 
-        return new Route($pattern, array('_controller' => 'SnowcapAdminBundle:Content:' . $action, 'alias' => $admin->getAlias()));
+        preg_match('/(?:[A-Z](?:[A-Za-z0-9])+\\\)*(?:)[A-Z](?:[A-Za-z0-9])+Bundle/', get_class($admin), $matches);
+        $bundle = implode('', explode('\\', $matches[0]));
+        $section = ucfirst($admin->getAlias());
+        $controller = $bundle . ':' . $section . ':' . $action;
+        try {
+            $controller = $this->parser->parse($controller);
+        }
+        catch(\InvalidArgumentException $e) {
+            $controller = $this->parser->parse('SnowcapAdminBundle:Content:' . $action);
+        }
+
+        return new Route($pattern, array('_controller' => $controller, 'alias' => $admin->getAlias()));
     }
 
     /**
