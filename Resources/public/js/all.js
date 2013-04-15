c995fca0e733d4073fb80c1555f89c4c976ab3fb
jerome <jerome@snowcap.be>
2013-04-15 11:24:17 +0200

    Using the beforeunload event trigger instead of custom code

--- Resources/public/js/all.js
+++ Resources/public/js/all.js
@@ -356,27 +356,21 @@ jQuery(document).ready(function ($) {
         $('body').data('admin-form-changed', true);
     });
 
-    $('a:not([href^=#])').click(function(event) {
-        var formHasChanged = $('body').data('admin-form-changed');
-        if(formHasChanged) {
-            event.preventDefault();
-            var href = $(this).attr('href');
-            var modal = $('#modal');
-            $.get(SNOWCAP_ADMIN_CONTENT_CHANGE_URL, function (data) {
-                modal.html(data);
-                modal.find('.cancel').click(function(event){
-                    modal.html('');
-                    modal.modal('hide');
-                });
-                modal.find('.proceed').click(function(event){
-                    window.location.href = href;
-                });
-                modal.find('.save').click(function(event){
-                    $('[data-admin=form-change-warning]').submit();
-                });
-                modal.modal('show');
-            });
-        }
-    });
+    // Handle "content changed" event
+    var $form_change_warning = $('[data-admin=form-change-warning]');
+    if ($form_change_warning.length > 0) {
+        $('body').data('admin-form-changed', false);
+        $form_change_warning.change(function(event) {
+            $('body').data('admin-form-changed', true);
+        });
+
+        $(window).bind('beforeunload', function() {
+            var formHasChanged = $('body').data('admin-form-changed');
+            if(formHasChanged) {
+                return $form_change_warning.data('changed-message');
+            }
+        });
+    }
+
 
 });
