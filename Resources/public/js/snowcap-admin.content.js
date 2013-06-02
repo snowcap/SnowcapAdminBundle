f41e53b14c8e0a3a35982af8d9b13185b89fad78
pvanliefland <pierre@snowcap.be>
2013-06-02 14:09:08 +0200

    Improved autocomplete and collection form type widgets

--- Resources/public/js/snowcap-admin.content.js
+++ Resources/public/js/snowcap-admin.content.js
@@ -1,198 +1,46 @@
 SnowcapAdmin.Content = (function() {
+    /**
+     * Admin modal form view
+     *
+     */
     var Modal = SnowcapBootstrap.Modal.extend({
-        $form: null,
-        events: function() {
-            return _.extend(SnowcapBootstrap.Modal.prototype.events, {
-                'submit form': 'submitForm',
-                'click a[data-admin=content-delete]': 'delete'
-            });
-        },
         initialize: function() {
             SnowcapBootstrap.Modal.prototype.initialize.apply(this);
-        },
-        submitForm: function(event) {
-            event.preventDefault();
-            var $form = this.$el.find('form');
-            $.post($form.attr('action'), $form.serialize(), null, "json")
-                .done(_.bind(this.done, this))
-                .fail(_.bind(this.fail, this));
-        },
-        delete: function(event) {
-            event.preventDefault();
-            var $link = $(event.currentTarget);
-            $.getJSON($link.attr('href'))
-                .done(_.bind(function(data) {
-                    this.$el.html(data.content);
-                }, this));
-        },
-        done: function(data) {
-            this.close();
-            this.trigger('content:modal:success', data.result);
-        },
-        fail: function(jqXHR, textStatus) {
-            switch(jqXHR.status) {
-                case 301: // REDIRECTION
-                    var response = JSON.parse(jqXHR.responseText);
-                    window.location.href = response.redirect_url;
-                    break;
-                default:
-                    console.log(jqXHR);
-                    console.log(textStatus);
-                    break;
-            }
-        },
-        postRender: function() {
-            SnowcapBootstrap.Modal.prototype.postRender.apply(this);
-            this.$('[data-admin=content-autocomplete]').each(function(offset, autocompleteContainer) { // TODO: a better way ?
-                new SnowcapAdmin.Content.Autocomplete({el: $(autocompleteContainer)});
-            });
+            this.off('modal:render');
+            this.on('modal:render', SnowcapAdmin.Form.collectionFactory);
+            this.on('modal:render', SnowcapAdmin.Form.autocompleteFactory);
         }
     });
 
-    var Autocomplete = Backbone.View.extend({
-        $textInput: null,
-        $valueInput: null,
-        listUrl: null,
-        mode: null,
-        labels: [],
-        mapped: {},
-        events: {
-            'click a[data-admin=content-add]': 'add'
-        },
-        /**
-         * Autocomplete widget init
-         *
-         */
-        initialize: function () {
-            this.$el.css('position', 'relative');
-            this.$textInput = this.$el.find('input[type=text]');
-            this.$valueInput = this.$el.find('input[type=hidden]');
-            this.listUrl = this.$el.data('options-url');
-            this.mode = this.$el.data('options-mode');
-
-            // Initialize typeahead
-            this.$textInput.typeahead({
-                source: _.bind(this.source, this),
-                minLength: 3,
-                items: 10,
-                matcher: _.bind(this.matcher, this),
-                updater: _.bind(this.updater, this)
-            });
-            // Handle focus / blur
-            this.$textInput.on('focus', function(){
-                $(this).data('prev', $(this).val());
-                $(this).val('');
-            }).on('blur', function(){
-                if($(this).val() === '') {
-                    $(this).val($(this).data('prev'));
+    /**
+     * Modal factory function
+     *
+     * @param $context
+     */
+    var modalFactory = function($context) {
+        $context.find('[data-admin=content-modal]').each(function(offset, modalTrigger) {
+            var $modalTrigger = $(modalTrigger);
+            $($modalTrigger).on('click', function(event) {
+                event.preventDefault();
+                var options = {
+                    url: $modalTrigger.attr('href')
+                };
+                if($modalTrigger.data('options-modal-class')) {
+                    options.modalClass = $modalTrigger.data('options-modal-class');
                 }
+                new SnowcapAdmin.Content.Modal(options);
             });
-            // Remove associations
-            if('multiple' === this.mode) {
-                $('ul.tokens').on('click', 'a[rel=remove]', _.bind(function(event) {
-                    event.preventDefault();
-                    var value = $(this).parent('li').data('value');
-                    $(this).parent('li').remove();
-                    this.$el.find('input[value=' + value + ']').remove();
-                }, this));
-            }
-        },
-
-        source: function(query, process) {
-            var replacedUrl = this.listUrl.replace('__query__', query);
-            $.getJSON(replacedUrl, _.bind(function(data) {
-                this.mapped = {};
-                this.labels = [];
-                $.each(data.result, _.bind(function (i, item) {
-                    this.mapped[item[1]] = item[0];
-                    this.labels.push(item[1]);
-                }, this));
-                process(this.labels);
-            }, this));
-        },
-
-        matcher: function(item) {
-            var existingTokens = this.$el.find('.token span').map(function() {
-                return $(this).html();
-            });
-
-            return -1 === $.inArray(item, existingTokens);
-        },
-
-        updater: function(item) {
-            if('single' === this.mode) {
-                this.$el.find('input[type=hidden]').val(this.mapped[item]).trigger('change');
-                return item;
-            }
-            else {
-                var prototype = this.$el.data('prototype');
-                var $prototype = $(prototype.replace(/__name__/g, this.$el.find('input[type=hidden]').length));
-                $prototype.val(this.mapped[item]);
-                this.$el.prepend($prototype);
-                $prototype.trigger('change');
-
-                $token = $('<li>').addClass('token').html($('<span>').html(item)).append($('<a>').html('&times;').addClass('close').attr('rel', 'remove'));
-                this.$el.find('.tokens').append($token);
-
-                return "";
-            }
-        },
-
-        add: function(event) {
-            event.preventDefault();
-            var $trigger = $(event.currentTarget);
-            var contentModal = new SnowcapAdmin.Content.Modal({url: $trigger.attr('href')});
-            contentModal.on('content:modal:success', _.bind(function(result){
-                var
-                    option = $('<option>'),
-                    entity_id = result.entity_id,
-                    entity_name = result.entity_name;
-                if('single' === this.mode) {
-                    // TODO refactor with autocomplete updater
-                    this.$textInput.val(entity_name);
-                    this.$valueInput.val(entity_id).trigger('change');
-                }
-                else {
-                    var prototype = this.$el.data('prototype');
-                    var $prototype = $(prototype.replace(/__name__/g, this.$el.find('input[type=hidden]').length));
-                    $prototype.val(entity_id);
-                    this.$el.prepend($prototype);
-                    $prototype.trigger('change');
-
-                    var $token = $('<li>').addClass('token').html($('<span>').html(entity_name)).append($('<a>').html('&times;').addClass('close').attr('rel', 'remove'));
-                    this.$el.find('.tokens').append($token);
-                }
-            }, this));
-        }
-    });
+        });
+    };
 
     return {
-        'Modal': Modal,
-        'Autocomplete': Autocomplete
+        Modal: Modal,
+        modalFactory: modalFactory
     }
 })();
 
 (function($) {
-    /**
-     * Observe modal triggers and create modal
-     * instances on click
-     *
-     */
-    $('[data-admin=content-modal]').each(function(offset, modalTrigger) {
-        var $modalTrigger = $(modalTrigger);
-        $($modalTrigger).on('click', function(event) {
-            event.preventDefault();
-            var options = {
-                url: $modalTrigger.attr('href')
-            };
-            if($modalTrigger.data('options-modal-class')) {
-                options.modalClass = $modalTrigger.data('options-modal-class');
-            }
-            new SnowcapAdmin.Content.Modal(options);
-        });
-    });
 
-    $('[data-admin=content-autocomplete]').each(function(offset, autocompleteContainer) {
-        new SnowcapAdmin.Content.Autocomplete({el: $(autocompleteContainer)});
-    });
+    SnowcapAdmin.Content.modalFactory($('body'));
+
 })(jQuery);
--- Resources/public/js/snowcap-admin.form.js
+++ Resources/public/js/snowcap-admin.form.js
@@ -1,52 +1,235 @@
-SnowcapAdmin.Form = (function() {
-    var Collection = Backbone.View.extend({
-        $container: null,
-        dataPrototype: null,
+SnowcapAdmin.Form = (function($) {
+    /**
+     * Form Collection view
+     * Used to manage Symfony form collection type
+     *
+     */
+    var Collection = SnowcapCore.Form.Collection.extend({
         events: {
-            'click .add-element': 'addElement',
-            'click *[data-admin=form-collection-add]': 'addElement',
-            'click .remove-element': 'removeElement',
-            'click [data-admin=form-collection-remove]': 'removeElement'
+            'click .add-element': 'addItem', // Legacy
+            'click *[data-admin=form-collection-add]': 'addItem',
+            'click .remove-element': 'removeItem', // Legacy
+            'click [data-admin=form-collection-remove]': 'removeItem'
         },
+        /**
+         * Initialize
+         *
+         */
         initialize: function() {
-            this.$widget = this.$el.find('[data-prototype]');
-            this.dataPrototype = this.$widget.data('prototype');
-            this.on('form:collection:add', this.observeAddedForm);
+            SnowcapCore.Form.Collection.prototype.initialize.apply(this);
+
+            this.on('form:collection:add', autocompleteFactory);
         },
-        removeElement: function(event) {
+        /**
+         * Remove a collection item
+         *
+         * @param event
+         */
+        removeItem: function(event) {
             event.preventDefault();
-            var $target = $(event.currentTarget);
-            var $parentCollectionItem = $target.parents('[data-admin=form-collection-item]');
-            if($parentCollectionItem.length > 0) {
-                $parentCollectionItem.remove();
+            var
+                $target = $(event.currentTarget),
+                $collectionItem;
+
+            $collectionItem = $target.parents('[data-admin=form-collection-item]');
+            if(0 === $collectionItem.length) {
+                $collectionItem = $target.parent();
+            }
+
+            if(this.options.confirmDelete) {
+                var modal = new SnowcapBootstrap.Modal({'url': this.options.confirmDeleteUrl});
+                modal.on('modal:confirm', function() {
+                    $collectionItem.remove();
+                });
             }
             else {
-                $target.parent().remove();
+                $collectionItem.remove();
             }
-        },
-        addElement: function(event) {
-            event.preventDefault();
-            var $form = $($.trim(this.dataPrototype.replace(/__name__/g, this.$widget.children().length)));
-            this.$widget.append($form);
-            this.trigger('form:collection:add', $form);
-        },
-        observeAddedForm: function($form) {
-            $form.find('[data-admin=content-autocomplete]').each(function(offset, autocompleteContainer) {
-                new SnowcapAdmin.Content.Autocomplete({el: $(autocompleteContainer)});
-            });
         }
     });
 
-    return {
-        'Collection': Collection
-    }
-})();
-
-(function($) {
     /**
-     * Init collections
+     * Form collection factory function
+     *
+     * @param $context
      */
-    $('[data-admin=form-collection-container]').each(function(offset, container) {
-        new SnowcapAdmin.Form.Collection({el: $(container)});
+    var collectionFactory = function() {
+        var $context = (0 === arguments.length) ? $('body') : arguments[0];
+        $context.find('[data-admin=form-collection]').each(function(offset, container) {
+            var options = {
+                el: $(container),
+                confirmDelete: $(container).data('options-confirm-delete-url') ? true : false,
+                confirmDeleteUrl: $(container).data('options-confirm-delete-url') ? $(container).data('options-confirm-delete-url') : null
+            };
+
+            new SnowcapAdmin.Form.Collection(options);
+        });
+    };
+
+    /**
+     * Form Autocomplete view
+     * Used to handle snowcap_admin_autocomplete form type
+     *
+     */
+    var Autocomplete = Backbone.View.extend({
+        $textInput: null,
+        $valueInput: null,
+        listUrl: null,
+        mode: null,
+        labels: [],
+        mapped: {},
+        events: {
+            'click a[data-admin=content-add]': 'add'
+        },
+        /**
+         * Initialize
+         *
+         */
+        initialize: function () {
+            this.$el.css('position', 'relative');
+            this.$textInput = this.$el.find('input[type=text]');
+            this.$valueInput = this.$el.find('input[type=hidden]');
+            this.listUrl = this.$el.data('options-url');
+            this.mode = this.$el.data('options-mode');
+
+            // Initialize typeahead
+            this.$textInput.typeahead({
+                source: _.bind(this.source, this),
+                minLength: 3,
+                items: 10,
+                matcher: _.bind(this.matcher, this),
+                updater: _.bind(this.updater, this)
+            });
+            // Handle focus / blur
+            this.$textInput.on('focus', function(){
+                $(this).data('prev', $(this).val());
+                $(this).val('');
+            }).on('blur', function(){
+                    if($(this).val() === '') {
+                        $(this).val($(this).data('prev'));
+                    }
+                });
+            // Remove associations
+            if('multiple' === this.mode) {
+                $('ul.tokens').on('click', 'a[rel=remove]', _.bind(function(event) {
+                    event.preventDefault();
+                    var value = $(this).parent('li').data('value');
+                    $(this).parent('li').remove();
+                    this.$el.find('input[value=' + value + ']').remove();
+                }, this));
+            }
+        },
+        /**
+         * Bootstrap typeahead source implementation
+         *
+         * @param query
+         * @param process
+         */
+        source: function(query, process) {
+            var replacedUrl = this.listUrl.replace('__query__', query);
+            $.getJSON(replacedUrl, _.bind(function(data) {
+                this.mapped = {};
+                this.labels = [];
+                $.each(data.result, _.bind(function (i, item) {
+                    this.mapped[item[1]] = item[0];
+                    this.labels.push(item[1]);
+                }, this));
+                process(this.labels);
+            }, this));
+        },
+        /**
+         * Bootstrap typeahed matcher implementation
+         *
+         * @param item
+         * @returns {boolean}
+         */
+        matcher: function(item) {
+            var existingTokens = this.$el.find('.token span').map(function() {
+                return $(this).html();
+            });
+
+            return -1 === $.inArray(item, existingTokens);
+        },
+        /**
+         * Bootstrap typeahed updater implementation
+         *
+         * @param item
+         * @returns {*}
+         */
+        updater: function(item) {
+            if('single' === this.mode) {
+                this.$el.find('input[type=hidden]').val(this.mapped[item]).trigger('change');
+                return item;
+            }
+            else {
+                var prototype = this.$el.data('prototype');
+                var $prototype = $(prototype.replace(/__name__/g, this.$el.find('input[type=hidden]').length));
+                $prototype.val(this.mapped[item]);
+                this.$el.prepend($prototype);
+                $prototype.trigger('change');
+
+                $token = $('<li>').addClass('token').html($('<span>').html(item)).append($('<a>').html('&times;').addClass('close').attr('rel', 'remove'));
+                this.$el.find('.tokens').append($token);
+
+                return "";
+            }
+        },
+        /**
+         * Add a new entity and select it within the widget
+         *
+         * @param event
+         */
+        add: function(event) {
+            event.preventDefault();
+            var $trigger = $(event.currentTarget);
+            var contentModal = new SnowcapAdmin.Content.Modal({url: $trigger.attr('href')});
+            contentModal.on('modal:success', _.bind(function(result){
+                var
+                    option = $('<option>'),
+                    entity_id = result.entity_id,
+                    entity_name = result.entity_name;
+                if('single' === this.mode) {
+                    // TODO refactor with autocomplete updater
+                    this.$textInput.val(entity_name);
+                    this.$valueInput.val(entity_id).trigger('change');
+                }
+                else {
+                    var prototype = this.$el.data('prototype');
+                    var $prototype = $(prototype.replace(/__name__/g, this.$el.find('input[type=hidden]').length));
+                    $prototype.val(entity_id);
+                    this.$el.prepend($prototype);
+                    $prototype.trigger('change');
+
+                    var $token = $('<li>').addClass('token').html($('<span>').html(entity_name)).append($('<a>').html('&times;').addClass('close').attr('rel', 'remove'));
+                    this.$el.find('.tokens').append($token);
+                }
+            }, this));
+        }
     });
+
+    /**
+     * Autocomplete factory function
+     *
+     * @param $context
+     */
+    var autocompleteFactory = function() {
+        var $context = (0 === arguments.length) ? $('body') : arguments[0];
+        $context.find('[data-admin=content-autocomplete]').each(function(offset, autocompleteContainer) {
+            new SnowcapAdmin.Form.Autocomplete({el: $(autocompleteContainer)});
+        });
+    };
+
+    return {
+        Collection: Collection,
+        collectionFactory: collectionFactory,
+        Autocomplete: Autocomplete,
+        autocompleteFactory: autocompleteFactory
+    }
 })(jQuery);
+
+jQuery(document).ready(function() {
+
+    SnowcapAdmin.Form.collectionFactory();
+    SnowcapAdmin.Form.autocompleteFactory();
+
+});
