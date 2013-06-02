35efd7083177bc2c7d99d0d156ce3abd70d8a232
pvanliefland <pierre@snowcap.be>
2013-06-02 14:10:23 +0200

    Added confirm modal for collection item removal

--- Controller/WidgetController.php
+++ Controller/WidgetController.php
@@ -3,9 +3,19 @@
 namespace Snowcap\AdminBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class WidgetController extends BaseController
 {
+    public function deleteItemAction()
+    {
+        $responseData = array(
+            'content' => $this->renderView('SnowcapAdminBundle:Widget:deleteItem.html.twig')
+        );
+
+        return new JsonResponse($responseData);
+    }
+
     /**
      * @Template
      */
