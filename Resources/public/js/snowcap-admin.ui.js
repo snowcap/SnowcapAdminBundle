213fc154191aaa5675860df79e78c003ba41b7da
jmsche <contact@jmsche.fr>
2013-08-07 14:14:48 +0200

    Fixed several modals opening

--- Resources/public/js/snowcap-admin.ui.js
+++ Resources/public/js/snowcap-admin.ui.js
@@ -7,9 +7,9 @@ SnowcapAdmin.Ui = (function() {
         initialize: function() {
             SnowcapBootstrap.Modal.prototype.initialize.apply(this);
             this.off('ui:modal:render');
-            this.on('ui:modal:render', SnowcapAdmin.Form.collectionFactory);
-            this.on('ui:modal:render', SnowcapAdmin.Form.textAutocompleteFactory);
-            this.on('ui:modal:render', SnowcapAdmin.Form.autocompleteFactory);
+            this.on('ui:modal:render', _.partial(SnowcapAdmin.Form.collectionFactory, this.$el));
+            this.on('ui:modal:render', _.partial(SnowcapAdmin.Form.textAutocompleteFactory, this.$el));
+            this.on('ui:modal:render', _.partial(SnowcapAdmin.Form.autocompleteFactory, this.$el));
             this.on('ui:modal:render', this.checkAlerts);
             this.on('ui:modal:success', this.success);
         },
