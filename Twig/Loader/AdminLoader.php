ab0063e87dba1bfe59bd995c057615d93281a9a3
Jérôme Poskin <jerome@snowcap.be>
2015-12-15 13:48:33 +0100

    Update AdminLoader.php

--- Twig/Loader/AdminLoader.php
+++ Twig/Loader/AdminLoader.php
@@ -9,7 +9,7 @@ class AdminLoader extends FilesystemLoader {
      * @param string $template
      * @return string
      */
-    protected function findTemplate($template)
+    protected function findTemplate($template, $throw = true)
     {
         $parts = explode(':', $template);
         $parts[1] = 'Content';
